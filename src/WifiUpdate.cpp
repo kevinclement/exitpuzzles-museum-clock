c2d278f61807b5fef85fd8a8fbea76c0b12cf441
Kevin Clement <kevin.clement@gmail.com>
2019-10-05 19:09:46 -0700

    turn wifi back on

--- src/WifiUpdate.cpp
+++ src/WifiUpdate.cpp
@@ -1,11 +1,11 @@
 #include "WifiUpdate.h"
 #include "logic.h"
 #include <WiFi.h>
-// #include <WiFiUdp.h>
-// #include <ArduinoOTA.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-// const char* ssid = "ExitPuzzles";
-// const char* password = "bossdj311";
+const char* ssid = "ExitPuzzles";
+const char* password = "bossdj311";
 
 WifiUpdate::WifiUpdate(Logic &logic)
 : _logic(logic)
@@ -15,37 +15,37 @@ WifiUpdate::WifiUpdate(Logic &logic)
 void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info)
 {
   Serial.println("Connected to wifi!");
-  // ArduinoOTA.begin();
+  ArduinoOTA.begin();
 }
 
 void WifiUpdate::setup() {
   
-  // WiFi.mode(WIFI_STA);
-  // WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
-  // WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_STA);
+  WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.begin(ssid, password);
 
-  // // setup wifi updates
-  // ArduinoOTA.setHostname("museum-clock");
-  // ArduinoOTA
-  //   .onStart([]() {
-  //     Serial.println("Start updating...");
-  //   })
-  //   .onEnd([]() {
-  //     Serial.println("\r\nEnd");
-  //   })
-  //   .onProgress([](unsigned int progress, unsigned int total) {
-  //     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  //   })
-  //   .onError([](ota_error_t error) {
-  //     Serial.printf("Error[%u]: ", error);
-  //     if (error == OTA_AUTH_ERROR)         Serial.println("Auth Failed");
-  //     else if (error == OTA_BEGIN_ERROR)   Serial.println("Begin Failed");
-  //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-  //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-  //     else if (error == OTA_END_ERROR)     Serial.println("End Failed");
-  //   });
+  // setup wifi updates
+  ArduinoOTA.setHostname("museum-clock");
+  ArduinoOTA
+    .onStart([]() {
+      Serial.println("Start updating...");
+    })
+    .onEnd([]() {
+      Serial.println("\r\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR)         Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR)   Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR)     Serial.println("End Failed");
+    });
 }
 
 void WifiUpdate::handle() {
-  // ArduinoOTA.handle();
+  ArduinoOTA.handle();
 }
