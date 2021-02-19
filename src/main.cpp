d109f76dc88fb8bd6a5749f6f9ed97c65d82dce6
Kevin Clement <kevin.clement@gmail.com>
2021-02-19 01:05:56 -0800

    padding removed in setup

--- src/main.cpp
+++ src/main.cpp
@@ -76,17 +76,17 @@ void setup() {
   logic.serial.print("Museum Grandfather clock by kevinc...\n");
   Serial.println(getFullVersion("museum-clock"));
 
-  logic.serial.registerCommand(SerialCommand("status",         's', &status,          "status",         "gets the status of device"));
-  logic.serial.registerCommand(SerialCommand("solve",          'v', &solve,           "solve",          "force a puzzle solve of the device"));
-  logic.serial.registerCommand(SerialCommand("debug",          'x', &debug,           "debug",          "toggle debugging of encoders"));
-  logic.serial.registerCommand(SerialCommand("encoder",        'e', &toggleEncoder,   "toggleEncoder",  "toggle heads encoder readings"));
-  logic.serial.registerCommand(SerialCommand("hour",           'h', &hourIncrement,   "hour",           "increment hour count as an override"));
-  logic.serial.registerCommand(SerialCommand("hourDec",        'o', &hourDec,         "hourDec",        "decrement hour count as an override"));
-  logic.serial.registerCommand(SerialCommand("reset",          'a', &resetHands,      "reset",          "reset the hands to the starting positions"));
-  logic.serial.registerCommand(SerialCommand("minute",         'm', &minuteIncrement, "minute",         "increment minute count as an override"));
-  logic.serial.registerCommand(SerialCommand("minDec",         'i', &minuteDec,       "minDec",         "decrement minute count as an override"));
-  logic.serial.registerCommand(SerialCommand("reboot",         'r', &reboot,          "reboot",         "software reboot the device"));
-  logic.serial.registerCommand(SerialCommand("audio",          'z', &audio,           "audio",          "play the solve audio"));
+  logic.serial.registerCommand(SerialCommand("status",   's', &status,          "status",         "gets the status of device"));
+  logic.serial.registerCommand(SerialCommand("solve",    'v', &solve,           "solve",          "force a puzzle solve of the device"));
+  logic.serial.registerCommand(SerialCommand("debug",    'x', &debug,           "debug",          "toggle debugging of encoders"));
+  logic.serial.registerCommand(SerialCommand("encoder",  'e', &toggleEncoder,   "toggleEncoder",  "toggle heads encoder readings"));
+  logic.serial.registerCommand(SerialCommand("hour",     'h', &hourIncrement,   "hour",           "increment hour count as an override"));
+  logic.serial.registerCommand(SerialCommand("hourDec",  'o', &hourDec,         "hourDec",        "decrement hour count as an override"));
+  logic.serial.registerCommand(SerialCommand("reset",    'a', &resetHands,      "reset",          "reset the hands to the starting positions"));
+  logic.serial.registerCommand(SerialCommand("minute",   'm', &minuteIncrement, "minute",         "increment minute count as an override"));
+  logic.serial.registerCommand(SerialCommand("minDec",   'i', &minuteDec,       "minDec",         "decrement minute count as an override"));
+  logic.serial.registerCommand(SerialCommand("reboot",   'r', &reboot,          "reboot",         "software reboot the device"));
+  logic.serial.registerCommand(SerialCommand("audio",    'z', &audio,           "audio",          "play the solve audio"));
 
   logic.serial.printHelp();
   logic.status();
